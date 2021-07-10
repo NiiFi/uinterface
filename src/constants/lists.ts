92e173462a3f4256fec0a8cc7dbfe8d7484d78b8
Hein Dauven <heindauven@gmail.com>
2021-07-10 13:27:14 +0200

    Rename nahmii list and standardize tokenlists.

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,11 +1,11 @@
 // used to mark unsupported tokens, these are hosted lists of unsupported tokens
 
-const NAHMII_LIST = '....../nahmiiList.json' // to be updated
+const DEFAULT_TOKEN_LIST = './tokenLists/niifi-default.tokenlist.json' // to be updated
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
 
 // lower index == higher priority for token import
-export const DEFAULT_LIST_OF_LISTS: string[] = [NAHMII_LIST]
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [NAHMII_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [DEFAULT_TOKEN_LIST]
