29da616b7ed1d6678b2e2a17225ea936ac53cadb
Taimoor Ali <tali@vodworks.com>
2021-07-15 00:56:46 +0500

    OM-213 implemented the value change in PoolInvestModal.

--- src/components/PoolInvestModal/index.tsx
+++ src/components/PoolInvestModal/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useCallback, useState, useMemo } from 'react'
 import styled from 'styled-components/macro'
 import { ReactComponent as Close } from '../../assets/images/x.svg'
 import { ApplicationModal } from '../../state/application/actions'
-import { useModalOpen, usePoolInvestModalToggle } from '../../state/application/hooks'
+import { useModalOpen, usePoolInvestModalToggle, useWalletModalToggle } from 'state/application/hooks'
 import { Trans } from '@lingui/macro'
+
+import { shortenDecimalValues } from 'utils'
+import { TOKEN_VALUE_CURRENCY_FORMAT } from 'constants/tokens'
 import { RowBetween } from 'components/Row'
 import { AutoColumn } from 'components/Column'
+import { useCurrency } from 'hooks/Tokens'
+import { useCurrencyBalance } from 'state/wallet/hooks'
+import { useActiveWeb3React } from 'hooks/web3'
 import TokenPairInputPanel from 'components/pools/TokenPairInputPanel'
 import Modal from '../Modal'
 import { TYPE } from 'theme'
 import { MainCurrency } from 'utils'
 import Slippage from 'components/swap/Slippage'
+import { PoolInvestPairValues } from 'state/pool/actions'
+import { useFakePoolValuesCalculator } from 'state/pool/hooks'
+import { tryParseAmount } from 'state/swap/hooks'
 import { ButtonPrimary } from 'components/Button'
+import { formatCurrencyAmount } from 'utils/formatCurrencyAmount'
 
 const HeaderRow = styled.div`
   font-weight: 500;
@@ -69,10 +79,30 @@ const Wrapper = styled.div`
 `
 
 export default function PoolInvestModal() {
-  const walletModalOpen = useModalOpen(ApplicationModal.POOL_INVEST)
+  const { account } = useActiveWeb3React()
+  const [token0Amount, setToken0Amount] = useState('')
+  const [investmentValue, setInvestmentValue] = useState('')
+  const { calculateTotalInvestmentInUSD } = useFakePoolValuesCalculator()
+  const inputCurrency = useCurrency('ETH')
+  const balance = useCurrencyBalance(account ?? undefined, inputCurrency ?? undefined)
+  const toggleWalletModal = useWalletModalToggle()
+  const poolInvestModalOpen = useModalOpen(ApplicationModal.POOL_INVEST)
   const togglePoolInvestModal = usePoolInvestModalToggle()
+  const handlePairValueChange = useCallback(
+    ({ token0, token1 }: PoolInvestPairValues) => {
+      setToken0Amount(token0.value)
+      setInvestmentValue(calculateTotalInvestmentInUSD({ token0, token1 }))
+    },
+    [setToken0Amount, calculateTotalInvestmentInUSD, setInvestmentValue]
+  )
+  const inputAmount = useMemo(
+    () => tryParseAmount(token0Amount, inputCurrency ?? undefined),
+    [inputCurrency, token0Amount]
+  )
+  const hasInputAmount = Boolean(inputAmount?.greaterThan('0'))
+  const sufficientBalance = inputAmount && balance && !balance.lessThan(inputAmount)
   return (
-    <Modal isOpen={walletModalOpen} onDismiss={togglePoolInvestModal} minHeight={false} maxHeight={90}>
+    <Modal isOpen={poolInvestModalOpen} onDismiss={togglePoolInvestModal} minHeight={false} maxHeight={90}>
       <Wrapper>
         <UpperSection>
           <CloseIcon onClick={togglePoolInvestModal}>
@@ -86,26 +116,45 @@ export default function PoolInvestModal() {
               <Trans>Amount to Invest</Trans>
             </TYPE.subHeader>
             <TYPE.subHeader color="text6">
-              <Trans>Balance 3,965</Trans>
-              {` ${MainCurrency}`}
+              {balance ? (
+                <>
+                  <Trans>Balance</Trans>
+                  {` ${formatCurrencyAmount(balance, 4)}`}
+                </>
+              ) : (
+                ''
+              )}
             </TYPE.subHeader>
           </RowBetween>
           <AutoColumn style={{ paddingTop: '1rem' }}>
             <TokenPairInputPanel
+              onChange={handlePairValueChange}
               token0={{ symbol: 'ETH', address: '12345' }}
               token1={{ symbol: 'NII', address: '12345' }}
             />
           </AutoColumn>
-          <TYPE.subHeader color="text6" textAlign="right">
-            <Trans>≈ 9,394.85</Trans>
-            {` ${MainCurrency}`}
-          </TYPE.subHeader>
+          <RowBetween marginTop="0.25rem">
+            <TYPE.error fontSize="0.875rem" fontWeight="normal" error={true} textAlign="left">
+              {!sufficientBalance && hasInputAmount ? <Trans>Insufficient Balance</Trans> : ''}
+            </TYPE.error>
+            <TYPE.subHeader color="text6" textAlign="right" width="50%">
+              {`≈ ${
+                investmentValue ? shortenDecimalValues(investmentValue, TOKEN_VALUE_CURRENCY_FORMAT) : '0'
+              } ${MainCurrency}`}
+            </TYPE.subHeader>
+          </RowBetween>
           <RowBetween marginTop="1rem">
             <Slippage placement={'left'} />
           </RowBetween>
-          <ButtonPrimary marginTop="1rem">
-            <Trans>INVEST</Trans>
-          </ButtonPrimary>
+          {account ? (
+            <ButtonPrimary disabled={!sufficientBalance && hasInputAmount} marginTop="1rem">
+              <Trans>INVEST</Trans>
+            </ButtonPrimary>
+          ) : (
+            <ButtonPrimary marginTop="1rem" onClick={toggleWalletModal}>
+              <Trans>Connect Wallet</Trans>
+            </ButtonPrimary>
+          )}
         </UpperSection>
       </Wrapper>
     </Modal>
