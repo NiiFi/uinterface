848f30fb261fd74fa0b6540691f3f1a2d0858926
Taimoor Ali <tali@vodworks.com>
2021-07-15 03:18:35 +0500

    OM-213 Synced two Input props.

--- src/components/pools/TokenPairInputPanel.tsx
+++ src/components/pools/TokenPairInputPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
 import { PoolInvestPair, PoolInvestPairValues } from 'state/pool/actions'
 import NumericalInput from 'components/NumericalInput'
 import CurrencyAvatar from 'components/CurrencyAvatar'
@@ -14,61 +14,39 @@ export default function TokenPairInputPanel({ token0, token1, onChange }: TokenP
   const { getValueEquivalentTo } = useFakePoolValuesCalculator()
   const [token0Value, setToken0Value] = useState('')
   const [token1Value, setToken1Value] = useState('')
-  const handleToken0OnBlur = useCallback(() => {
-    if (token0Value) {
-      const token1NextValue = getValueEquivalentTo(token0, token0Value, token1)
-      setToken1Value(token1NextValue)
-      onChange &&
-        onChange({
-          token0: {
-            ...token0,
-            value: token0Value,
-          },
-          token1: {
-            ...token1,
-            value: token1NextValue,
-          },
-        })
+  const getChangeHandler = (active: 'token0' | 'token1') => (value: string) => {
+    if (!value) {
+      setToken0Value('')
+      setToken1Value('')
+      onChange && onChange({ token0: { ...token0, value: '' }, token1: { ...token1, value: '' } })
+      return
     }
-  }, [token0Value, token0, token1, setToken1Value, onChange, getValueEquivalentTo])
+    const token0NextValue = active === 'token0' ? value : getValueEquivalentTo(token1, value, token0)
+    const token1NextValue = active === 'token1' ? value : getValueEquivalentTo(token0, value, token1)
+    setToken0Value(token0NextValue)
+    setToken1Value(token1NextValue)
+    onChange &&
+      onChange({ token0: { ...token0, value: token0NextValue }, token1: { ...token1, value: token1NextValue } })
+  }
 
-  const handleToken1OnBlur = useCallback(() => {
-    if (token1Value) {
-      const token0NextValue = getValueEquivalentTo(token1, token1Value, token0)
-      setToken0Value(token0NextValue)
-      onChange &&
-        onChange({
-          token0: {
-            ...token0,
-            value: token0NextValue,
-          },
-          token1: {
-            ...token1,
-            value: token1Value,
-          },
-        })
-    }
-  }, [token1Value, token0, token1, setToken0Value, onChange, getValueEquivalentTo])
   return (
     <>
       <RowBetween marginBottom="1rem">
         <CurrencyAvatar symbol={token0.symbol} containerStyle={{ padding: '0.3125rem' }} />
         <NumericalInput
           style={{ maxWidth: '50%' }}
-          onBlur={handleToken0OnBlur}
           fontSize="1.25rem"
           value={token0Value}
-          onUserInput={setToken0Value}
+          onUserInput={getChangeHandler('token0')}
         />
       </RowBetween>
       <RowBetween>
         <CurrencyAvatar symbol={token1.symbol} />
         <NumericalInput
           style={{ maxWidth: '50%' }}
-          onBlur={handleToken1OnBlur}
           fontSize="1.25rem"
           value={token1Value}
-          onUserInput={setToken1Value}
+          onUserInput={getChangeHandler('token1')}
         />
       </RowBetween>
     </>
