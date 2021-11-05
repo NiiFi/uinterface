fdf3197cf9a381ee43e199dddeb091104eab51b3
Lint Action <lint-action@samuelmeuli.com>
2021-11-05 11:06:39 +0000

    Fix code style issues with ESLint

--- src/utils/getExplorerLink.test.ts
+++ src/utils/getExplorerLink.test.ts
@@ -2,7 +2,9 @@ import { ExplorerDataType, getExplorerLink } from './getExplorerLink'
 
 describe('#getExplorerLink', () => {
   it('correct for tx Nahmii testnet', () => {
-    expect(getExplorerLink(5553, 'abc', ExplorerDataType.TRANSACTION)).toEqual('https://explorer.testnet.nahmii.io/tx/abc')
+    expect(getExplorerLink(5553, 'abc', ExplorerDataType.TRANSACTION)).toEqual(
+      'https://explorer.testnet.nahmii.io/tx/abc'
+    )
   })
   it('correct for token Nahmii testnet', () => {
     expect(getExplorerLink(5553, 'abc', ExplorerDataType.TOKEN)).toEqual('https://explorer.testnet.nahmii.io/token/abc')
